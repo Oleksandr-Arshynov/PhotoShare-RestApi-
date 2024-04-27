211d9ec335087eb977e018e068e0c2354481c2aa
Oleksandr Arshynov <oleksandrarshynov@gmail.com>
2024-04-27 07:40:35 +0300

    change docker compose

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,19 +2,20 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str
-    secret_key: str
-    algorithm: str
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
+    sqlalchemy_database_url: str = "sqlalchemy"
+    secret_key: str = "secret_key"
+    algorithm: str = "algorithms"
+    mail_username: str = "mail"
+    mail_password: str= "password"
+    mail_from: str = "mail@gmail.com"
+    mail_port: int = 3452
+    mail_server: str = "postgres"
     redis_host: str = 'localhost'
     redis_port: int = 6379
-    cloudinary_name: str
-    cloudinary_api_key: str
-    cloudinary_api_secret: str
+    redis_password: str = None
+    cloudinary_name: str = "claud"
+    cloudinary_api_key: str = "api_key"
+    cloudinary_api_secret: str = "api_secret"
 
     class Config:
         env_file = ".env"
