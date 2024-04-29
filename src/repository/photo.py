ce45d609c2fb7885f43ded9f8f5fbd7b4093dccc
Max <maxkryvytskyiwork@gmail.com>
2024-04-29 11:44:07 +0300

    save fix

--- src/repository/photo.py
+++ src/repository/photo.py
@@ -13,96 +13,97 @@ import cloudinary.uploader
 import uuid
 
 
-async def create_photo(
-    user_id: int,
-    photo_url: str,
-    description: str,
-    tags: List[str],
-    public_id: str,
-    db: Session,
-) -> Photo:
-    photo = Photo(
-        photo=photo_url, description=description, user_id=user_id, public_id=public_id
-    )
+# Встановлюємо конфігурацію Cloudinary
+cloudinary.config(
+    cloud_name=settings.CLD_NAME, api_key=settings.CLD_API_KEY, api_secret=settings.CLD_API_SECRET
+)
 
-    if photo:
+
+async def create_photo(user_id: int, file: UploadFile, description: str, tags: list, db: Session) -> Photo: # db: AsyncSession
+    # Отримуємо завантажений файл та опис
+    contents = await file.read()
+
+    # Завантажуємо файл в Cloudinary
+    response = cloudinary.uploader.upload(
+        contents,
+        folder=f"uploads/{user_id}",  # Папка, куди буде завантажено фото
+        public_id=str(uuid.uuid4()),  # Ім'я файлу на Cloudinary
+        description=description,
+        tags=tags
+    )
+    # Отримуємо URL завантаженого фото з відповіді Cloudinary
+    photo_url = response["secure_url"]
+    public_id = response["public_id"]
+    
+    photo = Photo(photo=photo_url, description=description, user_id=user_id, public_id=public_id)
+    
+    if photo: # перевіряє що photo вдало створено
         db.add(photo)
         db.commit()
-        tags = await repository_tag.create_tag(photo_id=photo.id, tags=tags, db=db)
-        photo = db.query(Photo).filter(Photo.id == photo.id).first()
+        tags = await repository_tag.create_tag(photo_id=photo.id, tags=tags, db=db) # додає теги
+        photo = db.query(Photo).filter(Photo.id==photo.id).first()
 
-        for num in range(0, len(tags)):
+        for num in range(0, len(tags)): # без цього не повертає теги 
             photo.tags[num].name
 
-    return photo
+    return photo 
 
 
-async def put_photo(
-    user_id: int, photo_id: int, photo: str, description: str, tags: list, db: Session
-) -> Tag:
-    post_photo = (
-        db.query(Photo)
-        .filter(and_(Photo.user_id == user_id, Photo.id == photo_id))
-        .first()
-    )
+async def put_photo(user_id: int, photo_id: int, file: UploadFile, description: str, tags: list, db: Session) -> Tag:
+    post_photo = db.query(Photo).filter(and_(Photo.user_id==user_id, Photo.id==photo_id)).first()
 
-    if post_photo:  # перевіряє що photo знайдено вдало
-        if photo:  # перевіряє що photo не пусте
-            post_photo.photo = photo
+    if post_photo: # перевіряє що photo знайдено вдало
+        contents = await file.read()
+        filename = file.filename
+        cloudinary.uploader.destroy(post_photo.public_id)
+        # Завантажуємо файл в Cloudinary
+        response = cloudinary.uploader.upload(
+            contents,
+            folder=f"uploads/{user_id}",  # Папка, куди буде завантажено фото
+            public_id=str(uuid.uuid4()),  # Ім'я файлу на Cloudinary
+            description=description,
+            tags=tags
+        )
+        # Отримуємо URL завантаженого фото з відповіді Cloudinary
+        post_photo.photo = response["secure_url"]
+        post_photo.public_id = response["public_id"]
 
-        if description:  # перевіряє що description не пусте
+        if description: # перевіряє що description не пусте 
             post_photo.description = description
 
-        if tags:  # перевіряє що tags не пусте
-
-            photo_tag = (
-                db.query(PhotoTagAssociation)
-                .filter(PhotoTagAssociation.photo_id == photo_id)
-                .all()
-            )
-            for el in photo_tag:  # видаляє старі теги
+        if tags: # перевіряє що tags не пусте
+            
+            photo_tag = db.query(PhotoTagAssociation).filter(PhotoTagAssociation.photo_id==photo_id).all()
+            for el in photo_tag: # видаляє старі теги
                 db.delete(el)
             db.commit()
 
-            tags = await repository_tag.create_tag(
-                photo_id=photo_id, tags=tags, db=db
-            )  # додає тові теги
+            tags = await repository_tag.create_tag(photo_id=photo_id, tags=tags, db=db) # додає тові теги
 
-            for num in range(
-                0, len(tags)
-            ):  # без цього не повертає теги, а просто {} або взягалі нічого
+            for num in range(0, len(tags)): # без цього не повертає теги, а просто {} або взягалі нічого
                 post_photo.tags[num].name
 
-        post_photo.updated_at = datetime.now()  # дата редагування
+        post_photo.updated_at = datetime.now() # дата редагування
 
     return post_photo
 
 
-async def delete_photo(
-    user_id: int, photo_id: int, db: Session
-) -> Photo | HTTPException:
-    photo = (
-        db.query(Photo)
-        .filter(and_(Photo.user_id == user_id, Photo.id == photo_id))
-        .first()
-    )
+async def delete_photo(user_id: int, photo_id: int, db: Session) -> Photo | HTTPException:
+    photo = db.query(Photo).filter(and_(Photo.user_id==user_id, Photo.id==photo_id)).first()
+
     if photo: # перевіряє що photo знайдено вдало
         cloudinary.uploader.destroy(photo.public_id)
         tags = await repository_tag.get_tags(photo_id=photo.id, db=db)
-        db.delete(photo)
+        db.delete(photo) 
         db.commit()
 
-        for num in range(
-            0, len(tags)
-        ):  # без цього не повертає теги, а просто {} або взягалі нічого
+        for num in range(0, len(tags)): # без цього не повертає теги, а просто {} або взягалі нічого
             photo.tags[num].name
-
-        return photo  # повертає видалений єлемент
-
-    else:  # якщо photo не знайдено викликає помилку 404
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found"
-        )
+        
+        return photo # повертає видалений єлемент 
+    
+    else: # якщо photo не знайдено викликає помилку 404
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found")
 
 
 async def get_photo(user_id: int, photo_id: int, db: Session) -> Photo | HTTPException:
